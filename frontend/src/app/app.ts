a562496df6af3e3e300c5bd94c5942ce713ac2f6
Muhamed Nur Beck <m.nur.beck@hotmail.de>
2025-07-11 20:38:45 +0200

    add deployed backend URL

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Element } from './element/element';
 import { CommonModule } from '@angular/common';
-
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
--- frontend/src/app/services/elementService.ts
+++ frontend/src/app/services/elementService.ts
@@ -5,9 +5,18 @@ import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ElementService {
-  private apiUrl = 'http://localhost:3000/api/elements';
+
+  private apiUrl = this.getApiUrl();
 
   constructor(private http: HttpClient) { }
+
+  private getApiUrl(): string {
+    // In Production: Render URL, sonst localhost
+    const isProduction = window.location.hostname !== 'localhost';
+    return isProduction
+      ? 'https://periodensystem-backend.onrender.com/api/elements'
+      : 'http://localhost:3000/api/elements';
+  }
 
   getAllElements(): Observable<IElement[]> {
     return this.http.get<IElement[]>(this.apiUrl);
